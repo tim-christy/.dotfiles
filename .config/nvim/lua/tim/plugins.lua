4b44a13200ad346d852ffd0e9b622d0be8e0bb91
tim-christy <tim@timchristy.io>
2022-12-10 23:01:00 +0000

    Add code to bootstrap packer and add autocmd to PackerSync on save

--- .config/nvim/lua/tim/plugins.lua
+++ .config/nvim/lua/tim/plugins.lua
@@ -1,12 +1,69 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+-- Bootstrap packer if it's not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    print('Packer not installed. Installing now...')
+    fn.system({
+      'git',
+      'clone', 
+      '--depth',
+      '1', 
+      'https://github.com/wbthomason/packer.nvim',
+      install_path,
+    })
+    vim.cmd [[packadd packer.nvim]]
+    print('Installation complete. Quit and reopen neovim')
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Packer popup window rather than vertical split default
+compiled_path = vim.fn.stdpath('config') .. 'plugin/packer_compiled.lua'
+packer = require('packer')
+packer.init {
+  display = {
+    open_fn = function()
+      return require('packer.util').float {border = 'rounded'}
+    end
+  },
+  compiled_path = compiled_path,
+  git = {
+    clone_timeout = 300,
+  },
+  working_sym = "勒", -- The symbol for a plugin being installed/updated
+  error_sym = "✗", -- The symbol for a plugin with an error in installation/updating
+  done_sym = "✓", -- The symbol for a plugin which has completed installation/updating
+}
+
+
+-- Auto compile packer after saving
+local packer_group = vim.api.nvim_create_augroup('packer', {clear = true})
+vim.api.nvim_create_autocmd({'BufWritePost'}, {
+  pattern = 'plugins.lua',
+  group = packer_group,
+  callback = function()
+    dofile(vim.fn.expand("%"))  -- dofile reloads this script (%)
+    vim.cmd[[PackerSync]]
+  end,
+})
+
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- Colorschemes
+  -- colorschemes
   use 'EdenEast/nightfox.nvim'
   use 'sainnhe/gruvbox-material'
   use 'sainnhe/everforest'
-end)
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
