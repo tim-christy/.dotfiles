6ddc3d0e6a6627c3c39661a63bf1cbfbe85f5b0a
tim-christy <tim@timchristy.io>
2022-12-11 22:10:55 +0000

    Clean up mixed quoting in keymaps to use single quotes

--- .config/nvim/after/plugin/keymaps.lua
+++ .config/nvim/after/plugin/keymaps.lua
@@ -1,47 +1,55 @@
 local keymap = vim.api.nvim_set_keymap
-local default_opts = {noremap = true, silent = true}
-local expr_opts = {noremap = true, silent = true}
+local opts = {noremap = true, silent = true}
+-- local expr_opts = {noremap = true, silent = true}
 
 -- jk normal mode
-keymap("i", "jk", "<ESC>", default_opts)
-keymap("v", "jk", "<ESC>", default_opts)
-keymap("t", "jk", "<C-\\><C-n>", default_opts) -- terminal
+keymap('i', 'jk', '<esc>', opts)
+keymap('v', 'jk', '<esc>', opts)
+keymap('t', 'jk', '<c-\\><c-n>', opts)
 
 
 -- Center search results
-keymap("n", "n", "nzz", default_opts)
-keymap("n", "N", "Nzz", default_opts)
+keymap('n', 'n', 'nzz', opts)
+keymap('n', 'N', 'Nzz', opts)
 
 
 -- Better indenting
-keymap("v", "<", "<gv", default_opts)
-keymap("v", ">", ">gv", default_opts)
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
 
 
 -- Paste over currently selected text without yanking it - i.e. you can paste repeatedly now
-keymap("v", "p", '"_dP', default_opts)
+keymap('v', 'p', "'_dP", opts)
 
 -- Switch buffers
-keymap("n", "<S-h>", "<CMD>bprevious<CR>", default_opts)
-keymap("n", "<S-l>", "<CMD>bnext<CR>", default_opts)
+keymap('n', '<S-h>', '<cmd>bprevious<cr>', opts)
+keymap('n', '<S-l>', '<cmd>bnext<cr>', opts)
+keymap('n', '<S-x>', '<cmd>bdelete<cr>', opts)
 
 -- Switch Windows
-keymap("n", "<C-h>", "<C-w>h", default_opts)
-keymap("n", "<C-j>", "<C-w>j", default_opts)
-keymap("n", "<C-k>", "<C-w>k", default_opts)
-keymap("n", "<C-l>", "<C-w>l", default_opts)
+keymap('n', '<c-h>', '<c-w>h', opts)
+keymap('n', '<c-j>', '<c-w>j', opts)
+keymap('n', '<c-k>', '<c-w>k', opts)
+keymap('n', '<c-l>', '<c-w>l', opts)
 
 -- Cancel search highlighting
-keymap("n", "<ESC>", "<CMD>nohl<Bar><CMD>echo<CR>", default_opts)
+keymap('n', '<esc>', '<cmd>nohl<bar><cmd>echo<cr>', opts)
+
+-- Remove highlighting
+keymap('n', '<leader>hl', '<cmd>nohl<cr>', opts)
 
 -- Resizing panes
-keymap("n", "<Left>", "<CMD>vertical resize +1<CR>", default_opts)
-keymap("n", "<Right>", "<CMD>vertical resize -1<CR>", default_opts)
-keymap("n", "<Up>", "<CMD>resize -1<CR>", default_opts)
-keymap("n", "<Down>", "<CMD>resize +1<CR>", default_opts)
+keymap('n', '<left>', '<cmd>vertical resize +1<cr>', opts)
+keymap('n', '<right>', '<cmd>vertical resize -1<cr>', opts)
+keymap('n', '<up>', '<cmd>resize -1<cr>', opts)
+keymap('n', '<down>', '<cmd>resize +1<cr>', opts)
 
 -- Run a live server
-keymap('n', '<leader>rls', '<cmd>term live-server %<cr><cmd>bp<cr>', default_opts)
+keymap('n', '<leader>rls', '<cmd>term live-server %<cr><cmd>bp<cr>', opts)
+
+-- Re-load file
+keymap('n', '<leader>sf', '<cmd>luafile %<cr>', opts)
+
 -- Telescope
 keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
 keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
