71d40c9d63270f2cb163162f868b40a4854f7f00
tim-christy <tim@timchristy.io>
2022-12-11 22:12:55 +0000

    Rewrite init.lua in configs to auto-source all lua files in directory

--- .config/nvim/lua/tim/configs/init.lua
+++ .config/nvim/lua/tim/configs/init.lua
@@ -1,5 +1,34 @@
--- Source configuration files here
+-- Auto-requires all configuration files in configs directory
+-- Glob returns a string with each path separated by a new line
+-- vim.split() splits the string into a table
+local paths = vim.split(vim.fn.glob('~/.config/nvim/lua/tim/configs/*.lua'), '\n')
+
+-- Get the filenames and require them
+for _, path in ipairs(paths) do
+  path_split = vim.fn.split(path, '/')  --path is a string path_split is a table
+  file = string.gsub(path_split[#path_split], '%.lua?$', '')
+  if file ~= 'init' then
+    require('tim.configs.' .. file)
+  end
+end
+
+
+--[[
+-- LSP
 require('tim.configs.lsp')
+
+-- TS
 require('tim.configs.treesitter')
+
+-- NvimTree
 require('tim.configs.nvim-tree')
+
+-- Lualine
 require('tim.configs.lualine')
+
+-- Telescope
+require('tim.configs.telescope')
+
+-- Comments
+require('tim.cong
+--]]
