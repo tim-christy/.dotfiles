d4778f5b39b741c301c97c7d5db1b8365407acb5
tim-christy <tim@timchristy.io>
2022-12-12 11:09:14 -0600

    Remove after/

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,3 @@
--- Utils - needs to be required first
--- Contains a function that auto-requires everything in the configs file
-require('tim.core.utils').setup_configs()
-
 -- Import plugins file (i.e. packer)
 require('tim.core')
 
