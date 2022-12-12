1f0dfea7bdbcb9e3d2ecf13e939ae06d55db14b4
tim-christy <tim@timchristy.io>
2022-12-12 03:17:19 +0000

    Create function to require configs and add to top init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Utils - needs to be required first
+-- Contains a function that auto-requires everything in the configs file
+require('tim.core.utils').setup_configs()
+
 -- Import plugins file (i.e. packer)
 require('tim.plugins')
 
